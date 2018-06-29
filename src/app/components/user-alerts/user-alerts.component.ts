995720a7a8b69fec5dcdc54025e19a946d8d4b99
Donnie Ellstrom <donnie.ellstrom@freshconsulting.com>
2018-06-29 00:23:39 -0700

    Finalize landing page

--- src/app/components/user-alerts/user-alerts.component.ts
+++ src/app/components/user-alerts/user-alerts.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAlertsComponent implements OnInit {
 
+  alert: String = 'No afternoon or evening classes July 4th!';
+  alertStatus: Boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.getAlerts();
+  }
+
+  getAlerts() {
+    // TODO: Request alert from api and set boolean accordingly
+
+    if (this.alert) {
+      this.alertStatus = true;
+    }
   }
 
 }

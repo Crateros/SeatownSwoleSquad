6649f88221e96e9370fa9859dd1711a747c2176f
Donnie Ellstrom <donnie.ellstrom@freshconsulting.com>
2018-06-29 21:19:37 -0700

    Add mock data and remove whitespace

--- src/app/components/daily-workout/daily-workout.component.ts
+++ src/app/components/daily-workout/daily-workout.component.ts
@@ -10,11 +10,21 @@ export class DailyWorkoutComponent implements OnInit {
   options: Object = { weekday: 'long', month: 'long', day: 'numeric' };
   today: String = new Date().toLocaleDateString('en-us', this.options);
   isWod: Boolean = true;
+  wod: Object = {};
 
   constructor() { }
 
   ngOnInit() {
-    console.log(this.today);
+    this.getMockWorkout();
+  }
+
+  getMockWorkout() {
+    fetch('assets/mock-data/wod.json').then((response) => {
+      return response.json();
+    }).then((data) => {
+      this.wod = data;
+      console.log(this.wod);
+    });
   }
 
 }

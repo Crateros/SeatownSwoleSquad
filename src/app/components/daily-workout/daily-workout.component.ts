05f37f3117713f4ae10d86dd67c776729d3864dd
Donnie Ellstrom <donnie.ellstrom@freshconsulting.com>
2018-11-16 09:28:55 -0800

    Fix responsive layout

--- src/app/components/daily-workout/daily-workout.component.ts
+++ src/app/components/daily-workout/daily-workout.component.ts
@@ -22,21 +22,13 @@ export class DailyWorkoutComponent implements OnInit {
 
   ngOnInit() {
     this.getDailyWorkout();
-    // this.createDailyWorkout()
-    // .then(data => console.log(JSON.stringify(data)))
-    // .catch(error => console.log(error));
-  }
-
-  formatWorkout(workout) {
-    this.wod = (<any>Object).assign(this.wod, workout);
-    this.wod.workoutDescriptionArray = this.wod.workoutDescription.split(/\n/);
   }
 
   getDailyWorkout() {
     const pathParameter = new Date().toISOString().slice(0, 10);
     const url = 'https://dlt8hc5cfl.execute-api.us-west-2.amazonaws.com/Devo/workout/' + pathParameter;
 
-    // return fetch(url, {
+    // fetch(url, {
     //   method: 'GET',
     //   mode: 'no-cors',
     //   cache: 'no-cache',
@@ -48,38 +40,25 @@ export class DailyWorkoutComponent implements OnInit {
     //   referrer: 'no-referrer',
     // })
     // .then(response => response.json())
-    // .then((data) => {
-    //   console.log('this is data: ', data);
-    // });
+    // .then(data => {
+    //   this.formatWorkout(data);
+    // })
+    // .catch(error => console.error('Error getting daily workout: ', error));
 
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        this.formatWorkout(data);
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        this.formatWorkout(myJson);
       });
-
-    // fetch('https://dlt8hc5cfl.execute-api.us-west-2.amazonaws.com/Devo/workout/10-28-2018).then((response) => {
-    //   return response.json();
-    // }).then((data) => {
-    //   this.wod = data;
-    //   this.isWod = Object.keys(this.wod).length > 0;
-    //   this.wodDescriptionArray = data.description.split('\n');
-    // });
-  }
-
-
-  getMockSkill() {
-    fetch('assets/mock-data/skill.json').then((response) => {
-      return response.json();
-    }).then((data) => {
-      this.skillDescriptionArray = data.description.split('\n');
-    });
   }
 
   createDailyWorkout() {
     const url = 'https://dlt8hc5cfl.execute-api.us-west-2.amazonaws.com/Devo/workout/';
     const data = {
       'workoutDate': new Date().toISOString().slice(0, 10),
+      'skillDescription': '5x5 Bench\n1RM Bench\n10 Jump Squats',
       'workoutDescription': 'Pushups\nPullups\nSitups Bitch!',
       'workoutName': 'Zach Gets Jacked',
       'isWod': false
@@ -97,17 +76,19 @@ export class DailyWorkoutComponent implements OnInit {
       referrer: 'no-referrer',
       body: JSON.stringify(data)
     })
-    .then(response => response.json());
+    .then(response => response.json())
+    .catch(error => console.error('Error creating workout: ', error));
+  }
+
+  formatWorkout(workout) {
+    this.wod = (<any>Object).assign(this.wod, workout);
+    this.wod.workoutDescriptionArray = this.wod.workoutDescription.split(/\n/);
+    this.wod.skillDescriptionArray = this.wod.skillDescription.split(/\n/);
   }
 
   updateWodDescription(description) {
-    this.wodDescriptionArray = description.split('\n');
   }
 
   updateSkillDescription(description) {
-    this.skillDescriptionArray = description.split('\n');
   }
-
-
-
 }
